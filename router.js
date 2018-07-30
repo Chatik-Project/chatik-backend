5327d76ca40dd4b3920a3c76a0be43840a61b05b
stikbomb <a.molodid@gmail.com>
2018-07-30 09:17:54 +0300

    Правка html

--- router.js
+++ router.js
@@ -99,13 +99,13 @@ module.exports = (app, passport) => {
     })
 
     app.post('/login', passport.authenticate('local-login', {
-        successRedirect : '/profile', // redirect to the secure profile section
+        successRedirect : '/', // redirect to the secure profile section
         failureRedirect : '/login', // redirect back to the signup page if there is an error
         // failureFlash : true // allow flash messages
     }));
 
     app.post('/register', passport.authenticate('local-signup', {
-        successRedirect : '/profile', // redirect to the secure profile section
+        successRedirect : '/', // redirect to the secure profile section
         failureRedirect : '/signup', // redirect back to the signup page if there is an error
         // failureFlash : true // allow flash messages
     }));
